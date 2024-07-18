aa7288645a9fa9dbd534528e1ca402b6acc3f6fa
Jishnu-Saravanan <124544400+Jishnu-Saravanan@users.noreply.github.com>
2024-07-18 11:42:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
+# import streamlit as st
+# from streamlit_webrtc import webrtc_streamer, VideoTransformerBase
+# import av
+# import cv2
+
+
+# class VideoTransformer(VideoTransformerBase):
+#     def transform(self, frame):
+#         img = frame.to_ndarray(format="bgr24")
+#         # You can process the image here if needed
+#         return av.VideoFrame.from_ndarray(img, format="bgr24")
+
+
+# st.title("Real-time Camera Feed")
+
+# webrtc_streamer(key="example", video_transformer_factory=VideoTransformer)
 import streamlit as st
-from streamlit_webrtc import webrtc_streamer, VideoTransformerBase
-import av
-import cv2
-
-
-class VideoTransformer(VideoTransformerBase):
-    def transform(self, frame):
-        img = frame.to_ndarray(format="bgr24")
-        # You can process the image here if needed
-        return av.VideoFrame.from_ndarray(img, format="bgr24")
-
+from streamlit_webrtc import webrtc_streamer
 
 st.title("Real-time Camera Feed")
 
-webrtc_streamer(key="example", video_transformer_factory=VideoTransformer)
+webrtc_streamer(key="example")
